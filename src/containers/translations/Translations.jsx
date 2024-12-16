25e81ff04fc4194e3a9c2d1a83a806323ecb1081
KEVIN CROSS MINCHAKPU <113268445+KCMinchakpu@users.noreply.github.com>
2024-12-16 16:14:32 +0100

    commit
    Adding container, files and CSS files

---
+++ src/containers/translations/Translations.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './translations.css'
+
+const Translations = () => {
+  return (
+    <div>Translations</div>
+  )
+}
+
+export default Translations
---
+++ src/containers/whoweare/Whoweare.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './whoweare.css';
+
+const Whoweare = () => {
+  return (
+    <div>Who We Are</div>
+  )
+}
+
+export default Whoweare
