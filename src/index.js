e63524844275507d0fa1bc3bd2dd88e124baafb0
KEVIN CROSS MINCHAKPU <113268445+KCMinchakpu@users.noreply.github.com>
2024-12-16 17:33:51 +0100

    commit
    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  
+import ReactDOM from 'react-dom/client';
+import './index.css';  
 
 import App from './App';
 
-// This is the new way to render in React 18 and later
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
