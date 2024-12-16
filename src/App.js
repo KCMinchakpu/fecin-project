8fceb2b644d8952555e1a7848c8614634bfbb154
KEVIN CROSS MINCHAKPU <113268445+KCMinchakpu@users.noreply.github.com>
2024-12-16 15:21:44 +0100

    commit
    New updates

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Robotic and Kevin Cross</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';  
+
+import App from './App';
+
+// This is the new way to render in React 18 and later
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
